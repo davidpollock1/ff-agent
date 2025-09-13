3839ea43766a02b70c67882499b6575839617a66
David <davidpollockcsu@gmail.com>
2025-09-13 00:24:24 -0700

    init frontend.

---
+++ api/main.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+router = APIRouter()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/")
+async def root(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/home")
+async def home(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+app.include_router(router)
